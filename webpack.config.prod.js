b6173654f78ac30b38507a09c5227052e1320a54
Ardihysenii <Ardihyseni737@gmail.com>
2026-03-07 00:01:01 +0100

    Fix: set output to public and optimize images

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,15 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path'); // Shto këtë rresht lart
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'public'), // Kjo i tregon Webpack-ut t'i dërgojë te 'public'
+    filename: '[name].[contenthash].js', // Ndihmon me cache-in e browser-it
+    clean: true, // Pastron folderin e vjetër para build-it të ri
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
