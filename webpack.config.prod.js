bb91f821bf580ef4569dbb25e297547a9c29d63d
Ardihysenii <Ardihyseni737@gmail.com>
2026-03-15 01:39:07 +0100

    New Version Albania

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,28 +2,29 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path'); // Shto këtë rresht lart
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'public'), // Kjo i tregon Webpack-ut t'i dërgojë te 'public'
-    filename: '[name].[contenthash].js', // Ndihmon me cache-in e browser-it
-    clean: true, // Pastron folderin e vjetër para build-it të ri
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
   plugins: [
+    // 1. Ky plugin krijon index.html dhe shton logon automatikisht
     new HtmlWebpackPlugin({
       template: './index.html',
+      favicon: './img/Mercedes-Benz.png',
     }),
+
+    // 2. Ky plugin vetëm kopjon dosjet/skedarët ashtu siç janë
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
